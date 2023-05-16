0437e7d5b93994b3a68ac06bbcf6e4e7f6341f16
albrecht0210 <albrecht.lanojan0210@gmail.com>
2023-05-16 10:44:30 +0800

    Init Abstract Syntax Tree

---
+++ InterpreterAppV3.Library/Tree/Statement/DeclarationNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterAppV3.Library.Tree.Statement
+{
+    internal class DeclarationNode
+    {
+    }
+}
